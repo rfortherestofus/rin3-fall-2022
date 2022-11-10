8268b89644ecf4910b80d1f9e98c533d09cde100
David Keyes <david@rfortherestofus.com>
2022-11-10 11:50:11 -0800

    Post week 8 live session

--- misc/survey-data-tidying.R
+++ misc/survey-data-tidying.R
@@ -7,27 +7,20 @@ library(janitor)
 
 survey_monkey_data_raw <- read_excel("data/survey-monkey-data.xlsx")
 
-last_24_hours_activity <- survey_monkey_data_raw %>%
+survey_monkey_data_tidy <- survey_monkey_data_raw %>% 
   clean_names() %>% 
+  select(-x8) %>% 
+  # slice(-1)
   drop_na(start_date) %>% 
-  select(-c(start_date, x7, x8)) %>% 
-  pivot_longer(cols = select_all_the_things_youve_done_in_the_past_24hours:x6) %>% 
-  select(-name) %>% 
-  mutate(value = str_remove(value, "- ")) %>% 
-  drop_na(value)
+  pivot_longer(cols = -start_date,
+               names_to = "question",
+               values_to = "response") %>% 
+  select(-question) %>% 
+  drop_na(response) %>% 
+  mutate(response = str_remove(response, "- "))
 
-last_24_hours_activity
-
-survey_monkey_data_raw %>%
-  clean_names() %>% 
-  drop_na(start_date) %>% 
-  select(-start_date) %>% 
-  pivot_longer(cols = everything()) %>% 
-  select(-name) %>% 
-  mutate(value = str_remove(value, "- ")) %>% 
-  drop_na(value) %>% 
-  count(value) %>% 
-  view()
+survey_monkey_data_tidy %>% 
+  count(response)
 
 
 # Qualtrics ---------------------------------------------------------------
@@ -36,28 +29,11 @@ survey_monkey_data_raw %>%
 qualtrics_data_raw <- read_excel("data/qualtrics-data.xlsx",
                                  skip = 1)
 
-last_24_hours_questions <- qualtrics_data_raw %>%
-  clean_names() %>% 
-  separate_rows(select_all_the_things_youve_done_in_the_past_24hours_selected_choice,
-                sep = ",") 
-
-last_24_hours_questions %>% 
-  count(select_all_the_things_youve_done_in_the_past_24hours_selected_choice) %>% 
-  ggplot(aes(x = select_all_the_things_youve_done_in_the_past_24hours_selected_choice,
-             y = n)) +
-  geom_col()
 
 
 # Google Forms/Sheets -----------------------------------------------------
 
 google_sheets_raw <- read_excel("data/google-forms-data.xlsx")
-
-last_24_hours_questions_google <- google_sheets_raw %>%
-  clean_names() %>% 
-  separate_rows(select_all_the_things_youve_done_in_the_past_24hours,
-                sep = ", ") 
-
-last_24_hours_questions_google
 
 
 # Google Forms/Sheets Multiple Parts --------------------------------------
