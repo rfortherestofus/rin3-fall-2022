7d877b6ca6ee30221a5a4218d039c7fa2a04aefa
David Keyes <david@rfortherestofus.com>
2022-12-01 11:18:58 -0800

    Update stephanie-evergreen-ggtext.R

--- misc/stephanie-evergreen-ggtext.R
+++ misc/stephanie-evergreen-ggtext.R
@@ -8,5 +8,45 @@ kindergarten_readiness <- tribble(
   "Literacy", 34, "Fall",
   "Literacy", 69, "Spring",
   "Language", 67, "Fall",
-  "Language", 75, "Spring"
-)
+  "Language", 75, "Spring",
+  "Mathematics", 63, "Fall",
+  "Mathematics", 77, "Spring",
+  "Science", 92, "Fall",
+  "Science", 98, "Spring",
+  "Creative Arts", 96, "Fall",
+  "Creative Arts", 100, "Spring"
+) %>% 
+  mutate(subject = factor(subject,
+                          levels = c("Literacy", "Language", "Mathematics", "Science", "Creative Arts"))) %>% 
+  mutate(subject = fct_rev(subject))
+
+subject_labels <- kindergarten_readiness %>% 
+  filter(date == "Fall")
+
+kindergarten_readiness %>%
+  ggplot(aes(x = score,
+             y = subject,
+             color = date,
+             label = score)) +
+  geom_point(size = 8) +
+  geom_text(color = "white") +
+  geom_text(data = subject_labels,
+            aes(label = subject),
+            hjust = 1.5) +
+  scale_color_manual(values = c(
+    "Fall" = "#867c84",
+    "Spring" = "#c15250"
+  )) +
+  scale_x_continuous(limits = c(0, 100),
+                     breaks = seq(from = 0,
+                                  to = 100,
+                                  by = 10)) +
+  labs(title = "Kindergarten readiness increased between <span style='color: #867c84;'>Fall</span> and <span style='color: #be5250;'>Spring</span>") +
+  theme_minimal() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.title = element_blank(),
+        plot.title = element_markdown(),
+        axis.text.y = element_blank(),
+        legend.position = "none")
+  
